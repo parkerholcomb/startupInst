5bc5bc67823d61e537b49e3845c07fa5e31fb4fb
Parker Holcomb <parker.holcomb@gmail.com>
2016-08-23 20:33:44 -0400

    used jason's code to improve mine

--- ruby/week1/you_do/age.rb
+++ ruby/week1/you_do/age.rb
@@ -1,12 +1,10 @@
 # Write a program that asks for a name and an age.
 # Have it output something like “Bob was born in 1985”
 
-require 'date'
-
 puts "whats your name"
 name = gets.chomp
 
 puts "whats your age?"
 age = gets.chomp.to_i
 
-puts "#{name} was born in #{Date.today.year - age}"
+puts "#{name} was born in #{Time.now.year - age}"
