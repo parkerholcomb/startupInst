5bf6b5d69a007d4fa5d47d010d500a53d6094930
Parker Holcomb <parker.holcomb@gmail.com>
2016-10-04 19:21:06 -0400

    week 8 review init

---
+++ ruby/week8/review.rb
@@ -0,0 +1,58 @@
+
+# environments
+  # console
+  # terminal
+
+# concepts/tools
+  # executing scripts "ruby hello_world.rb" vs in console
+  # binding.pry
+  # git
+    # repository
+    # commits
+    # branches
+
+# methods
+  # naming method
+  # parameters
+  # return values
+  # local scope
+
+# data types
+  # string
+  # int
+  # array
+  # hash
+  # booleans
+
+# control statements
+  # if
+  # else
+
+# loops
+  # while
+  # until
+  # 10.times
+
+  # iterators
+    # each
+
+# Class
+  # defining a class
+  # initializing a class
+
+  # variables
+    # instance variables
+    # local variables
+    # class variables
+  # methods
+    # instance methods
+    # class methods
+
+  # inheritance
+    # subclass
+    # sharing/overriding methods
+
+# organization
+  # organization in files
+  # require 'Date'
+  # require 'open-uri'
